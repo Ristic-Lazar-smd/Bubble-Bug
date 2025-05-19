e25b33b526e05e0ef7c88e573755b2898bfc4c79
Rista <risticlazarsmd@gmail.com>
2025-05-19 21:21:11 +0200

    update dasher bube, zapocet scoreManager

--- Assets/ScoreManager.cs
+++ Assets/ScoreManager.cs
@@ -4,12 +4,17 @@ public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager Instance { get; private set; }
 
+    int currentScore;
+
     void Awake() {
         Instance = this;
     }
 
-    public void UpdateScore(){
-        
+    public void UpdateScore(int updateIncrement){
+
+    //currentScore+=updateIncrement*dificulty mod*roomspassed mod*special
     }
+
+    
 
 }
